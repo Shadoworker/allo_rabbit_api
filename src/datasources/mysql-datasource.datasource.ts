4fdfefb1b60f17089a824c134226303d233ba3f1
habibeshadow <habibe@kayfo.games>
2022-05-08 13:14:12 +0000

    Clever updated db

--- src/datasources/mysql-datasource.datasource.ts
+++ src/datasources/mysql-datasource.datasource.ts
@@ -2,15 +2,24 @@ import { inject, lifeCycleObserver, LifeCycleObserver } from '@loopback/core';
 import { juggler } from '@loopback/repository';
 
 const config = {
+  // name: 'mysqlDatasource',
+  // connector: 'mysql',
+  // url: 'mysql://root:root@127.0.0.1/allo_rabbit_db',
+  // host: '127.0.0.1',
+  // port: 3306,
+  // user: 'root',
+  // password: 'root',
+  // database: 'allo_rabbit_db'
   name: 'mysqlDatasource',
   connector: 'mysql',
-  url: 'mysql://root:root@127.0.0.1/allo_rabbit_db',
-  host: '127.0.0.1',
+  url: 'mysql://ula0ylvdnpcrh2xb:ZLotrWFtWhYd1f5uHyMs@bz5o4ncdepwymfxwnxbb-mysql.services.clever-cloud.com:3306/bz5o4ncdepwymfxwnxbb',
+  host: 'bz5o4ncdepwymfxwnxbb-mysql.services.clever-cloud.com',
   port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'allo_rabbit_db'
+  user: 'ula0ylvdnpcrh2xb',
+  password: 'ZLotrWFtWhYd1f5uHyMs',
+  database: 'bz5o4ncdepwymfxwnxbb'
 };
+
 
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
